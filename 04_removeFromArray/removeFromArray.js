5f6f23aa4213cb818061164936c23a61714f9acd
teakys <mateakys@gmail.com>
2025-02-11 17:18:15 -0300

    retard activities

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...remove) {
+    let result = array;
+    for (i in remove) { 
+    result = array.filter((item) => item !== remove[i]);
+    }
+    return idfk;
+}
 
+
+// const removeFromArray = function(array, remove) {
+//     let arr = []; 
+//     for (let i = 1;i <= array.length;i++) {
+//         if (i !== remove) {
+//         arr.push(i);
+//         }
+//     }
+//     return arr;
+// };
+console.log(removeFromArray([1, 2, 3, 4], 4, 3));
 // Do not edit below this line
 module.exports = removeFromArray;
+
