0270061d0720cdbba9e111abe65ec049960028fc
teakys <mateakys@gmail.com>
2025-02-12 10:57:48 -0300

    idk man susy

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x < 0|y < 0) return 'ERROR'; 
+    else if (!Number.isInteger(x)|!Number.isInteger(y)) return 'ERROR';
+    else if(x < y) {
+        for (;x <= y;x++) {
+            sum += x;
+        }
+    } else {
+        for (;y <= x;y++) {
+            sum += y;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
