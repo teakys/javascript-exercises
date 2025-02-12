6de821dc9be1379681bec995dc5fa29366350ae2
teakys <mateakys@gmail.com>
2025-02-12 17:42:58 -0300

    im sorry

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people[0].yearOfBirth
 };
-
+        console.log(findTheOldest([{
+          name: "Carly",
+          yearOfBirth: 1942,
+          yearOfDeath: 1970,
+        },
+        {
+          name: "Ray",
+          yearOfBirth: 1962,
+          yearOfDeath: 2011,
+        },
+        {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+      ]));
 // Do not edit below this line
 module.exports = findTheOldest;
