4afa8c5a60e277eaf4001612260b3f2f2a1c71a9
teakys <mateakys@gmail.com>
2025-02-12 17:43:28 -0300

    i am

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = []
+    for (let i = 0;i < books.length;i++) {
+        titles.push(books[i].title);
+    }
+    return titles;
+//    return books[0].title;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
