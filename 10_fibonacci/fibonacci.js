24a625a8e0cc9b049a3860d2a3b4ae0f637eaf15
teakys <mateakys@gmail.com>
2025-02-12 14:35:44 -0300

    this was fun

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+    if (int < 0) return 'OOPS'
+    let sequence = [];
+    sequence[0] = 0;
+    sequence[1] = 1;
+    for (let i = 2; i < 30; i++) {
+        sequence.push(+(sequence[i-1])+(+sequence[i-2]));
+    }
+    return sequence[int];
 };
-
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
