485c920ef30df27ec28f9d61648a1a5bb78b689d
teakys <mateakys@gmail.com>
2025-02-12 11:59:50 -0300

    blazingly fast calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(intX, intY) {
+    return intX + intY;
 };
 
-const subtract = function() {
-	
+const subtract = function(intX, intY) {
+    return intX - intY;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+    let sum = 0;
+    for (number of numbers) {
+        sum += number;
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+    let result = 1;
+    for (number of numbers) {
+        result *= number;
+    }
+    return result
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+    let result = 1;
+    for (let i = 0;i < exponent;i++) {
+        result *= base;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(integer) {
+    if (integer === 0) return 1;
+    for (let i = integer -1;i > 0;i--) {
+        integer *= i;
+    }
+    return integer;
 };
 
 // Do not edit below this line
