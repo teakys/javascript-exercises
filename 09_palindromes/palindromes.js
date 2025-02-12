ea5d0f45de9c4727d16850a5c47637c3ea7bb5c8
teakys <mateakys@gmail.com>
2025-02-12 12:53:48 -0300

    i might be a god

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
 
-};
+const palindromes = function (str) {
+    let splitReverse = str.split('').reverse().toString().replace(/[,.! ]/g, "").toLowerCase();
+    let strFinal = str.split('').toString().replace(/[,.! ]/g, "").toLowerCase();
+    if (splitReverse == strFinal) return true;
+    else return false
+//return splitReverse
 
+}
+//acaramanamaraca
+//const palindromes = function (str) {
+//    let splitReverse = (str.split('').filter((letter) => (letter !== ","|letter !== "."|letter !== " ")).reverse()).toString();
+//    return splitReverse;
+//    if (splitReverse.toLowerCase() == string.toLowerCase()) return true;
+//};
 // Do not edit below this line
 module.exports = palindromes;
